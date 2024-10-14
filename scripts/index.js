612f9dbb9a62643e709fb463259cfd0e99fb5569
Elena Gulisashvili <lenochka.gulisashvili@yandex.ru>
2024-10-14 18:11:26 +0300

    new changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// @todo: Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
+  const cardClone = cardTemplate.cloneNode(true);
+  const cardElement = cardClone.querySelector(".card");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCardCallback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Вывести карточки на страницу
+
+function renderCards(cards) {
+  cards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.appendChild(cardElement);
+  });
+}
+
+renderCards(initialCards);
